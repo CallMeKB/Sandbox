ef042f91f3658860a0666e21176f77216ea876fb
jc374181 <keegan.bentley@my.jcu.edu.au>
2017-03-13 11:17:52 +1000

    Add name printing script

--- oddName.py
+++ oddName.py
@@ -1,1 +1,13 @@
 ''' Keegan '''
+name = input("Enter your name:")
+while name == "":
+    print("You must enter your name.")
+    name = input("Enter your name:")
+letter = 1
+try:
+    for i in range(len(name)):
+        print(name[letter])
+        letter += 2
+
+except IndexError:
+    print("End of name (:")
